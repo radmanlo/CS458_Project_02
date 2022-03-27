401348199e3c9bbdf468bc545f7444d697b07e94
radmanlo <radman.lotfiazar@gmail.com>
2022-03-27 15:14:56 +0300

    working on getting message when it is empty

--- app/src/main/java/com/example/cs458_project_02/MainActivity.java
+++ app/src/main/java/com/example/cs458_project_02/MainActivity.java
@@ -2,19 +2,32 @@ package com.example.cs458_project_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText birthDate;
+    TextInputLayout name, city;
+    Spinner gender, vaccine, sideEffect, posTest;
+    Button submit;
+    TextView display;
+
     //listener for getting date form the componenet
     DatePickerDialog.OnDateSetListener datelistener;
 
@@ -45,5 +58,64 @@ public class MainActivity extends AppCompatActivity {
                 birthDate.setText(date);
             }
         };
+        name = findViewById(R.id.NameInput);
+        city = findViewById(R.id.CityInput);
+        gender = findViewById(R.id.gender);
+        sideEffect = findViewById(R.id.SideEffect);
+        posTest = findViewById(R.id.positiveTest);
+        submit = findViewById(R.id.button);
+        display = findViewById(R.id.textView);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String surname = name.getEditText().getText().toString();
+                String gen = gender.getSelectedItem().toString();
+                String birth = birthDate.getText().toString();
+                if (surname.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Name-Surname is Empty").
+                            setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+                else if (gen.equals("Gender")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Gender is Empty").
+                            setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+                else if (birth.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Birth date is Empty").
+                            setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+
+
+            }
+        });
+
+
+
+
+
     }
 }
